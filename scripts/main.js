06c41d38c02d7719e53f8e5c85119112eb7f9fcd
redfish <redfish0501@gmail.com>
2020-05-08 11:29:14 +0800

    整理重複程式碼

--- scripts/main.js
+++ scripts/main.js
@@ -7,22 +7,20 @@ var vm = new Vue({
    doneTasks: [] // 完成事項
   },
   methods: {
-    // 添加到頁面
     add_icon: function() {
-      if(this.newTask == ""){
+      if( this.newTask == "" ){
         alert("尚未輸入項目");
       }else{
-        vm.addWaititem(this.newTask);
+        vm.addWaititem( this.newTask );
         this.newTask = '';
       }
-    },
-    addWaititem: function(content) {
+    }, //add_icon() 添加到頁面
+    addWaititem: function( content ) {
       var time =  Math.floor(Date.now());
       this.waitTasks.push({ id: time, text: this.newTask, controllModify: true });
-      console.log("Push time:" + time);
-    },
-    // 顯示改為input模式
-    waitModifyIcon: function(id) {
+      console.log( "Push time:" + time );
+    }, //addWaititem()
+    waitModifyIcon: function( id ) {
       for(let n = 0; n < this.waitTasks.length ; n++ ){
         if( this.waitTasks[n].id === id ) {
           console.log( "In edit: " + id ); // check
@@ -32,63 +30,54 @@ var vm = new Vue({
           this.waitTasks[n].controllModify = true;
         }
       }
-    },
-    // 離開編輯模式
-    closeEdit: function(id, isComplete) {
-      for(let n = 0; n < this.waitTasks.length ; n++ ){
-        if( this.waitTasks[n].id === id ) {
-          if( isComplete ) {  // 確認修改資料
-            console.log( "Succeeded edit: " + id ); // check
-            this.waitTasks[n].text = this.tempTask;
-          }
-          console.log( "Return edit: " + id ); // check
-          this.tempTask = '';
-          this.waitTasks[n].controllModify = true;
-        }
+    }, //waitModifyIcon() 顯示改為input模式
+    closeEdit: function( id, isComplete ) {
+      let seat = this.taskSeeking( id, this.waitTasks );
+      if( isComplete ) {
+        console.log( "Succeeded edit: " + id ); // check
+        this.waitTasks[seat].text = this.tempTask;
       }
-    },
-    // 刪除
-    waitTrashIcon: function(id) {
-      for(let n = 0; n < this.waitTasks.length ; n++ ){
-        if( this.waitTasks[n].id === id ) {
-          console.log( "Delete wait: " + id ); // check
-          this.waitTasks.splice(n,1);
-        }
-      }
-    },
-    // 完成task
-    waitDoneIcon: function(id) {
-      for( let n = 0 ; n < this.waitTasks.length ; n++ ){
-        if( this.waitTasks[n].controllModify && this.waitTasks[n].id === id) {
-          this.addDoneTask(this.waitTasks[n]);
-          this.waitTrashIcon(id);
-          break;
-        }
-      }
-    },
-    // 新增至donetask
-    addDoneTask: function( task) {
+      console.log( "Return edit: " + id ); // check
+      this.tempTask = '';
+      this.waitTasks[seat].controllModify = true;
+    }, // closeEdit() 離開編輯模式
+    waitTrashIcon: function( id ) {
+      let seat = this.taskSeeking( id, this.waitTasks );
+      console.log( "Delete wait: " + id );  // check
+      this.waitTasks.splice( seat,1 );
+    }, // waitTrashIcon() 刪除
+    waitDoneIcon: function( id ) {
+      let seat = this.taskSeeking( id, this.waitTasks );
+      if( this.waitTasks[seat].controllModify ) {
+        this.addDoneTask( this.waitTasks[seat] );
+        this.waitTrashIcon( id );
+      } // if
+    }, // waitDoneIcon() 完成task
+    addDoneTask: function( task ) {
       console.log(task);
-      this.doneTasks.push(task);
-    },
-    // 移除已完成task
-    doneTrashIcon: function(id) {
-      for(let n = 0; n < this.doneTasks.length ; n++ ){
-        if( this.doneTasks[n].id === id ) {
-          console.log( "Delete done: " + id ); // check
-          this.doneTasks.splice(n,1);
+      this.doneTasks.push( task );
+    }, // addDoneTask() 新增至donetask
+    doneTrashIcon: function( id ) {
+      let seat = this.taskSeeking( id, this.doneTasks );
+      console.log( "Delete done: " + id ); // check
+      this.doneTasks.splice( seat,1 );
+    },  // doneTrashIcon() 移除已完成task
+    doneUndoneIcon: function( id ) {
+      let seat = this.taskSeeking(id, this.doneTasks);
+      console.log( "Done to Undone: " + id ); // check
+      this.waitTasks.push( this.doneTasks[seat] );
+      this.doneTasks.splice( seat,1 );
+    },  // doneUndoneIcon() 修改為待辦task
+    taskSeeking: function( id, task ) {
+      for(let n = 0; n < task.length ; n++ ){
+        if( task[n].id === id ) {
+          console.log( "Seek the " + id ); // check
+          return n;
         }
       }
-    },
-    // 修改為待辦task
-    doneUndoneIcon: function(id) {
-      for(let n = 0; n < this.doneTasks.length ; n++ ){
-        if( this.doneTasks[n].id === id ) {
-          console.log( "Done to Undone: " + id ); // check
-          this.waitTasks.push(this.doneTasks[n]);
-          this.doneTasks.splice(n,1);
-        }
-      }
-    }
-  }
+
+      console.log("Failed to seak " + id);
+      return -1;
+    } // taskSeeking() 尋找task array中等同id的task
+  } // methods
 })
